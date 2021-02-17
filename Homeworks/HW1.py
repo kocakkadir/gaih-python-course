3d9dd45703e30155704338ecd986bd04ed3a2779
Kadir Kocak <kocak.kdr@gmail.com>
2021-02-17 19:11:52 +0300

    Hw1

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,31 @@
-#Explain your work
+import random
 
-#Question 1
-for x in range(a):
-	print(a)
+# Initialize matrix 
+matrix = [] 
+# Initialize a list
+primes = []
+for possiblePrime in range(2, 100):
+    
+    # Assume number is prime until shown it is not. 
+    isPrime = True
+    for num in range(2, possiblePrime):
+        if possiblePrime % num == 0:
+            isPrime = False
+      
+    if isPrime:
+        primes.append(possiblePrime)
+
+
+R=3
+C=3
+for i in range(R):          # A for loop for row entries 
+    a =[] 
+    for j in range(C):      # A for loop for column entries 
+         a.append(int(random.choice(primes))) 
+    matrix.append(a) 
+  
+# For printing the matrix 
+for i in range(R): 
+    for j in range(C): 
+        print(matrix[i][j], end = " ") 
+    print() 
